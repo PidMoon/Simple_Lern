ce68d4cba1d664d038580ae1714c711c0ee712db
PidMoon <pidmoonq@gmail.com>
2019-08-17 23:00:38 +0300

    Add-default-project-structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App() {
+    let mission: string = 'Mission completed';
+
+    return (
+        <h1>
+            {mission}
+        </h1>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
